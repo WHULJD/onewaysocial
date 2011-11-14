f28c9cba3c06a9df88d3b407138cb23a189e258d
rightgenius <rightgenius@gmail.com>
2011-11-14 10:11:26 +0800

    manychange

---
+++ ceiling.js
@@ -0,0 +1,7 @@
+﻿// JavaScript Document
+$(document).ready(function(){
+	$("#combo ul").width($("#combo").width());
+	$("#combo ul li").width($("#combo").width());
+
+});
+
--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-/*!
+﻿/*!
  * jQuery JavaScript Library v1.6.4
  * http://jquery.com/
  *
